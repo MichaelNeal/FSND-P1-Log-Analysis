63c184a66f2a0f70a7497e2bdc5a9e0c7a963aac
Michael Neal <mn6729@att.com>
2018-08-19 10:06:09 -0400

    Fixed pycodestyle E722 Added more descriptive db connection error with traceback Changed query constants to triple quotation as suggested. Updated author heading

--- log_metrics.py
+++ log_metrics.py
@@ -6,29 +6,29 @@ import psycopg2
 DBNAME = "news"
 
 # Define Constants
-metric1_query = ("SELECT articles.title, count(*) as views "
-                 "FROM articles inner JOIN log ON log.path "
-                 "LIKE concat('%', articles.slug, '%') "
-                 "WHERE log.status LIKE '%200%' GROUP by "
-                 "articles.title, log.path ORDER by views DESC limit 3")
+metric1_query = """SELECT articles.title, count(*) as views "
+                "FROM articles inner JOIN log ON log.path "
+                "LIKE concat('/article/', articles.slug) "
+                "WHERE log.status LIKE '%200%' GROUP by "
+                "articles.title, log.path ORDER by views DESC limit 3"""
 
-metric2_query = ("SELECT authors.name, count(*) as views "
-                 "FROM articles JOIN authors ON articles.author = authors.id "
-                 "JOIN log ON articles.slug = substring(log.path, 10)"
-                 "WHERE log.status LIKE '200 OK' GROUP BY authors.name "
-                 "ORDER BY views DESC;")
+metric2_query = """SELECT authors.name, count(*) as views "
+                "FROM articles JOIN authors ON articles.author = authors.id "
+                "JOIN log ON articles.slug = substring(log.path, 10)"
+                "WHERE log.status LIKE '200 OK' GROUP BY authors.name "
+                "ORDER BY views DESC;"""
 
-metric3_query = ("WITH num_requests AS (SELECT time::date AS day, count(*)"
-                 "FROM log GROUP BY time::date ORDER BY time::date),"
-                 "num_errors AS ("
-                 "SELECT time::date AS day, count(*) FROM log "
-                 "WHERE status != '200 OK'"
-                 "GROUP BY time::date ORDER BY time::date), error_rate AS ("
-                 "SELECT num_requests.day, num_errors.count::float / "
-                 "num_requests.count::float * 100"
-                 "AS error_pc FROM num_requests, num_errors "
-                 "WHERE num_requests.day = num_errors.day"
-                 ") SELECT * FROM error_rate WHERE error_pc > 1;")
+metric3_query = """WITH num_requests AS (SELECT time::date AS day, count(*)"
+                "FROM log GROUP BY time::date ORDER BY time::date),"
+                "num_errors AS ("
+                "SELECT time::date AS day, count(*) FROM log "
+                "WHERE status != '200 OK'"
+                "GROUP BY time::date ORDER BY time::date), error_rate AS ("
+                "SELECT num_requests.day, num_errors.count::float / "
+                "num_requests.count::float * 100"
+                "AS error_pc FROM num_requests, num_errors "
+                "WHERE num_requests.day = num_errors.day"
+                ") SELECT * FROM error_rate WHERE error_pc > 1;"""
 
 # Define functions
 
@@ -38,8 +38,10 @@ def connect(database_name="news"):
         db = psycopg2.connect("dbname={}".format(database_name))
         c = db.cursor()
         return db, c
-    except:
+    except Exception as e:
         print ("Unable to connect to the database")
+        print ("Error: " + str(e))
+        print (traceback.format_exc())
 
 
 def popular_article():
@@ -58,7 +60,7 @@ def popular_authors():
     c.execute(metric2_query)
     result = c.fetchall()
     db.close()
-    print("\nTOP THREE AUTHORS BY VIEWS:")
+    print("\nTOP AUTHORS BY VIEWS:")
     for i in range(0, len(result), 1):
         print ("(" + str(i + 1) + ") \"" + result[i][0] +
                "\" - " + str(result[i][1]) + " views")
